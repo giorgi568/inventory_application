63c1b3636d42a1fce74bc47e95eb06f9b9648813
giorgi568 <mateshvili02@mail.ru>
2024-02-25 22:29:19 +0400

    added all crud operations for category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -28,6 +28,7 @@ exports.detail = async (req, res, next) => {
     res.render('category_detail', {
       categoryName: category.name,
       categoryDescription: category.description,
+      categoryUrl: category.url,
       items: itemsInCategory,
     });
   } catch (err) {
@@ -46,4 +47,147 @@ exports.create_post = [
     .trim()
     .isLength({ min: 3 })
     .escape(),
+  body('description', 'Category description must contain al least 3 characters')
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+
+      const category = new Category({
+        name: req.body.name,
+        description: req.body.description,
+      });
+
+      if (!errors.isEmpty()) {
+        res.render('category_form', {
+          title: 'Create Category',
+          category: category,
+          errors: errors.array(),
+        });
+        return;
+      } else {
+        const categoryExists = await Category.findOne({ name: req.body.name })
+          .collation({ locale: 'en', strength: 2 })
+          .exec();
+
+        if (categoryExists) {
+          res.redirect(categoryExists.url);
+        } else {
+          await category.save();
+          res.redirect(category.url);
+        }
+      }
+    } catch (err) {
+      return next(err);
+    }
+  },
 ];
+
+exports.update_get = async (req, res, next) => {
+  try {
+    const category = await Category.findById(req.params.id).exec();
+    res.render('category_form', {
+      title: 'Update Category',
+      category: category,
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+exports.update_post = [
+  body('name', 'Category name must contain at least 3 characters')
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body('description', 'Category description must contain al least 3 characters')
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+
+      const category = new Category({
+        name: req.body.name,
+        description: req.body.description,
+        _id: req.params.id,
+      });
+
+      if (!errors.isEmpty()) {
+        res.render('category_form', {
+          title: 'Create Category',
+          category: category,
+          errors: errors.array(),
+        });
+        return;
+      } else {
+        const categoryExists = await Category.findOne({ name: req.body.name })
+          .collation({ locale: 'en', strength: 2 })
+          .exec();
+
+        if (categoryExists) {
+          res.redirect(categoryExists.url);
+        } else {
+          console.log(req.params.id);
+          const updatedCategory = await Category.findByIdAndUpdate(
+            req.params.id,
+            category,
+            {}
+          );
+          res.redirect(updatedCategory.url);
+        }
+      }
+    } catch (err) {
+      return next(err);
+    }
+  },
+];
+
+exports.delete_get = async (req, res, next) => {
+  try {
+    const [category, allItemsByCategory] = await Promise.all([
+      Category.findById(req.params.id).exec(),
+      Item.find({ category: req.params.id }, 'name').exec(),
+    ]);
+
+    if (category === null) {
+      res.redirect('/');
+    }
+
+    res.render('category_delete', {
+      title: 'Delete Category',
+      category: category,
+      category_items: allItemsByCategory,
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+exports.delete_post = async (req, res, next) => {
+  try {
+    const [category, allItemsByCategory] = await Promise.all([
+      Category.findById(req.params.id).exec(),
+      Item.find({ category: req.params.id }, 'name').exec(),
+    ]);
+
+    if (allItemsByCategory.isLength > 0) {
+      res.resnder('category_delete', {
+        title: 'Delete Category',
+        category: category,
+        category_items: allItemsByCategory,
+      });
+      return;
+    } else {
+      await Category.findByIdAndDelete(req.params.id);
+      res.redirect('/');
+    }
+  } catch (err) {
+    return next(err);
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,13 @@ router.get('/categories/create', category_controller.create_get);
 router.post('/categories/create', category_controller.create_post);
 
 router.get('/categories/:id', category_controller.detail);
+
+router.get('/categories/:id/update', category_controller.update_get);
+router.post('/categories/:id/update', category_controller.update_post);
+
+router.get('/categories/:id/delete', category_controller.delete_get);
+router.post('/categories/:id/delete', category_controller.delete_post);
+
 // item routes
 router.get('/items/:id', item_controller.detail);
 
