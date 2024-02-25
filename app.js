9d890b6dd40cef622abcf8287a32a09b737b0f17
giorgi568 <mateshvili02@mail.ru>
2024-02-26 03:51:33 +0400

    made ready to deploy

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const compression = require('compression');
+const helmet = require('helmet');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -21,9 +24,19 @@ async function main() {
   await mongoose.connect(mongoDb);
 }
 
+app.use(compression());
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
 
 app.use(logger('dev'));
 app.use(express.json());
